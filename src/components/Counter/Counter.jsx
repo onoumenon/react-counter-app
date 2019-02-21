d5f1a1841683235388cfb6b7ceccb09767642f35
huitian <tanhuitian@gmail.com>
2019-02-21 13:50:21 +0800

    Part4: CreateCounters

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export class Counter extends Component {
-  state = { id: 1, value: 0 };
+  state = { id: this.props.id, value: this.props.value };
 
   handleIncrement = () => {
     // cannot mutate state directly, so create a new variable
@@ -10,9 +10,6 @@ export class Counter extends Component {
   };
 
   handleDecrement = () => {
-    if (this.state.value <= 0) {
-      return;
-    }
     // cannot mutate state directly, so create a new variable
     const newValue = this.state.value - 1;
     this.setState({ value: newValue });
@@ -24,21 +21,27 @@ export class Counter extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <button onClick={this.handleDecrement} className="btn btn-primary">
+      <div className="badge badge-light">
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-sm btn-primary"
+        >
           {" "}
-          -{" "}
+          +{" "}
         </button>
         <h2>
           <span className={`badge badge-${this.evaluateClass()}`}>
             {this.state.value}
           </span>
         </h2>
-        <button onClick={this.handleIncrement} className="btn btn-primary">
-          {" "}
-          +{" "}
+
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-sm btn-primary"
+        >
+          {"  "}-{"  "}
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 }
