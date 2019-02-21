07af0f64f701c46526bfde0a9318b20feb5b1351
huitian <tanhuitian@gmail.com>
2019-02-21 11:19:21 +0800

    Part2: Create Counter + and -buttons

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,11 +3,36 @@ import React, { Component } from "react";
 export class Counter extends Component {
   state = { id: 1, value: 0 };
 
+  handleIncrement = () => {
+    // cannot mutate state directly, so create a new variable
+    const newValue = this.state.value + 1;
+    this.setState({ value: newValue });
+  };
+  handleDecrement = () => {
+    if (this.state.value === 0) {
+      return;
+    } else {
+      // cannot mutate state directly, so create a new variable
+      const newValue = this.state.value - 1;
+      this.setState({ value: newValue });
+    }
+  };
+
   render() {
     return (
-      <h2>
-        <span class="badge badge-secondary">{this.state.value}</span>
-      </h2>
+      <React.Fragment>
+        <button onClick={this.handleDecrement} className="btn btn-primary">
+          {" "}
+          -{" "}
+        </button>
+        <h2>
+          <span className="badge badge-secondary">{this.state.value}</span>
+        </h2>
+        <button onClick={this.handleIncrement} className="btn btn-primary">
+          {" "}
+          +{" "}
+        </button>
+      </React.Fragment>
     );
   }
 }
