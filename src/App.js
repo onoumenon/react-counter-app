65e2edf6dcd9a095edcab4124ad3d2c176be5553
huitian <tanhuitian@gmail.com>
2019-02-21 10:49:11 +0800

    Part1: Create Counter component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Counter } from "./components/Counter/Counter";
 
 export class App extends Component {
   render() {
     return (
-      <h1>
-        Counter Lab
-      </h1>
-    )
+      <React.Fragment>
+        <h1>Counter Lab</h1>
+        <Counter />
+      </React.Fragment>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  state = { id: 1, value: 0 };
+
+  render() {
+    return (
+      <h2>
+        <span class="badge badge-secondary">{this.state.value}</span>
+      </h2>
+    );
+  }
+}
+
+export default Counter;
